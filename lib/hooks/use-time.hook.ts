96c9a476417659a25d1e86f7ad098cd599fc066d
funny-family <maksvadik228@gmail.com>
2023-07-06 19:43:33 +0700

    update "use-time" hook

--- lib/hooks/use-time.hook.ts
+++ lib/hooks/use-time.hook.ts
@@ -9,59 +9,50 @@ type UseTimeHookReturnValue = {
   hours: string;
   format: UseTimeHookArgFormat;
 };
-type UseTimeHook = (args: UseTimeHookArgs) => UseTimeHookReturnValue;
+type UseTimeHook = (args?: UseTimeHookArgs) => UseTimeHookReturnValue;
 
 /**
  * @description Displays current time
  */
-export const useTime: UseTimeHook = (args) => {
+export const useTime = ((args: UseTimeHookArgs) => {
   if (args.format == null) {
     args.format = '24-hour';
   }
 
-  const formatTimeString = (string: string) =>
-    string.length === 1 ? `0${string}` : string;
+  const getLocaleTimeString = (Date: DateConstructor) =>
+    new Date().toLocaleTimeString('en-US', {
+      hour12: args.format === '12-hour' ? true : false,
+    });
+  type GetLocaleTimeStringFunction = typeof getLocaleTimeString;
 
-  const date = { value: new Date() };
+  const date = {
+    value: getLocaleTimeString(Date),
+  };
   type DateValue = typeof date;
-  const seconds = (
-    (date: DateValue) => () =>
-      formatTimeString(date.value.getSeconds().toString())
-  )(date);
-  const minutes = (
-    (date: DateValue) => () =>
-      formatTimeString(date.value.getMinutes().toString())
-  )(date);
-  const hours = (
-    (date: DateValue) => () =>
-      formatTimeString(date.value.getHours().toString())
-  )(date);
 
   const timeStore = createStore<UseTimeHookReturnValue>({
-    seconds: seconds(),
-    minutes: minutes(),
-    hours: hours(),
+    seconds: `${date.value[6]}${date.value[7]}`,
+    minutes: `${date.value[3]}${date.value[4]}`,
+    hours: `${date.value[0]}${date.value[1]}`,
     format: args.format,
   });
-
   type TimeStore = typeof timeStore;
 
   const tick = (
     (
       timeStore: TimeStore,
       date: DateValue,
-      seconds: () => string,
-      minutes: () => string,
-      hours: () => string
+      Date: DateConstructor,
+      getLocaleTimeString: GetLocaleTimeStringFunction
     ) =>
     () => {
-      date.value = new Date();
+      date.value = getLocaleTimeString(Date);
 
-      timeStore[1]('seconds', seconds());
-      timeStore[1]('minutes', minutes());
-      timeStore[1]('hours', hours());
+      timeStore[1]('seconds', `${date.value[6]}${date.value[7]}`);
+      timeStore[1]('minutes', `${date.value[3]}${date.value[4]}`);
+      timeStore[1]('hours', `${date.value[0]}${date.value[1]}`);
     }
-  )(timeStore, date, seconds, minutes, hours);
+  )(timeStore, date, Date, getLocaleTimeString);
 
   const intervalID = setInterval(tick, 1000);
 
@@ -70,4 +61,4 @@ export const useTime: UseTimeHook = (args) => {
   });
 
   return timeStore[0];
-};
+}) as UseTimeHook;
