772a6675a546d703d74517beedaf4078853a860d
funny-family <maksvadik228@gmail.com>
2023-07-17 21:00:52 +0700

    update stopwatch utils

--- lib/hooks/use-stopwatch/utils.ts
+++ lib/hooks/use-stopwatch/utils.ts
@@ -1,15 +1,11 @@
-import { type StopwatchTimerInterface } from './stopwatch-timer.class';
-
-export const calculateMilliseconds = (
-  stopwatchTimer: StopwatchTimerInterface
-): string => `${stopwatchTimer.value || '00'}`.padStart(2, '0').slice(-2);
+export const calculateMilliseconds = (value: number): string =>
+  `${value || '00'}`.padStart(2, '0').slice(-2);
 export type CalculateMillisecondsFunction = typeof calculateMilliseconds;
 
-export const calculateSeconds = (
-  stopwatchTimer: StopwatchTimerInterface
-): string => `${~~((stopwatchTimer.value % 60000) / 1000)}`.padStart(2, '0');
+export const calculateSeconds = (value: number): string =>
+  `${~~((value % 60000) / 1000)}`.padStart(2, '0');
 export type CalculateSecondsFunction = typeof calculateSeconds;
 
-export const calculateMinutes = (stopwatchTimer: StopwatchTimerInterface) =>
-  `${~~(stopwatchTimer.value / 60000)}`.padStart(2, '0');
+export const calculateMinutes = (value: number) =>
+  `${~~(value / 60000)}`.padStart(2, '0');
 export type CalculateMinutesFunction = typeof calculateMinutes;
