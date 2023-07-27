b52aab58f339aa86cf4301cda38c3fe57d02298e
funny-family <maksvadik228@gmail.com>
2023-07-27 22:27:08 +0700

    update "app" component at playground

--- playground/src/app.component.tsx
+++ playground/src/app.component.tsx
@@ -1,29 +1,17 @@
-import { createSignal, type Component } from 'solid-js';
+import { type Component } from 'solid-js';
 import { UseStopwatchRoute } from './routes/use-stopwatch.route';
 import { UseTimeRoute } from './routes/use-time.route';
-// import { UseTimerRoute } from './routes/use-timer.route';
+import { UseTimerRoute } from './routes/use-timer.route';
 import './app.styles.css';
 
 export const App: Component = () => {
-  const [dateString, setDateString] = createSignal('');
-
-  // setInterval(() => {
-  //   const string = new Date().toLocaleTimeString('en-US', {
-  //     hour12: false,
-  //   });
-
-  //   setDateString(string);
-  // }, 1000);
-
   return (
     <main>
       <UseStopwatchRoute />
       <br />
       <UseTimeRoute />
       <br />
-      {/* <UseTimerRoute /> */}
-
-      {/* <div>dateString: {dateString()}</div> */}
+      <UseTimerRoute />
     </main>
   );
 };
