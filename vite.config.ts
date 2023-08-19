becbaaf63dcc5ebc937d39177a74f000a47afc1b
funny-family <maksvadik228@gmail.com>
2023-08-19 14:49:00 +0700

    update build options

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,52 @@ import solidPlugin from 'vite-plugin-solid';
 import dts from 'vite-plugin-dts';
 import path from 'node:path';
 
-export default defineConfig({
-  plugins: [
-    solidPlugin({
-      include: 'lib/**/*',
-      extensions: ['jsx', 'tsx', 'js', 'ts'],
-    }),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-  build: {
-    minify: false,
-    lib: {
-      entry: path.resolve(__dirname, './lib/index.ts'),
-      name: 'solid-js-timers',
-      formats: ['es', 'umd'],
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      input: {
-        entry: path.resolve(__dirname, './lib/index.ts'),
-      },
-      external: ['solid-js'],
-      output: {
-        exports: 'named',
-        globals: {
-          'solid-js': 'solidJs',
+export default defineConfig(() => {
+  const entry = path.resolve(__dirname, './lib/index.ts');
+
+  return {
+    plugins: [
+      solidPlugin({
+        include: 'lib/**/*',
+        extensions: ['jsx', 'tsx', 'js', 'ts'],
+      }),
+      dts({
+        rollupTypes: true,
+        outputDir: path.resolve(__dirname, './dist'),
+      }),
+    ],
+    build: {
+      target: 'esnext',
+      minify: false,
+      lib: {
+        entry,
+        name: 'solid-js-timers',
+        formats: ['es', 'cjs'],
+        fileName: (format) => {
+          if (format === 'es') {
+            return 'esm/index.js';
+          }
+
+          if (format === 'cjs') {
+            return 'cjs/index.js';
+          }
+
+          return '';
         },
       },
+      rollupOptions: {
+        input: {
+          entry,
+        },
+        external: ['solid-js'],
+        output: {
+          exports: 'named',
+          globals: {
+            'solid-js': 'solidJs',
+          },
+        },
+      },
+      emptyOutDir: false,
     },
-    emptyOutDir: false,
-  },
+  };
 });
