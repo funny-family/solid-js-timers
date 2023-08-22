fad35bc48b706dd8574d07bc641264d3a02fcf15
funny-family <maksvadik228@gmail.com>
2023-08-22 21:28:17 +0700

    update build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,45 @@
 import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
 import dts from 'vite-plugin-dts';
 import path from 'node:path';
+import pkg from './package.json';
+import tsconfig from './tsconfig.json';
 
-export default defineConfig(() => {
-  const entry = path.resolve(__dirname, './lib/index.ts');
+export default defineConfig({
+  plugins: [
+    dts({
+      rollupTypes: true,
+      outputDir: path.resolve(__dirname, './dist/types'),
+    }),
+  ],
+  build: {
+    target: tsconfig.compilerOptions.target,
+    minify: false,
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, './lib/index.ts'),
+      name: pkg.name,
+      formats: ['es', 'cjs'],
+      fileName: (format) => {
+        if (format === 'es') {
+          return 'esm/index.js';
+        }
 
-  return {
-    plugins: [
-      solidPlugin({
-        include: 'lib/**/*',
-        extensions: ['jsx', 'tsx', 'js', 'ts'],
-      }),
-      dts({
-        rollupTypes: true,
-        outputDir: path.resolve(__dirname, './dist'),
-      }),
-    ],
-    build: {
-      target: 'esnext',
-      minify: false,
-      lib: {
-        entry,
-        name: 'solid-js-timers',
-        formats: ['es', 'cjs'],
-        fileName: (format) => {
-          if (format === 'es') {
-            return 'esm/index.js';
-          }
+        if (format === 'cjs') {
+          return 'cjs/index.js';
+        }
 
-          if (format === 'cjs') {
-            return 'cjs/index.js';
-          }
-
-          return '';
-        },
+        return '';
       },
-      rollupOptions: {
-        input: {
-          entry,
-        },
-        external: ['solid-js'],
-        output: {
-          exports: 'named',
-          globals: {
-            'solid-js': 'solidJs',
-          },
-        },
-      },
-      emptyOutDir: false,
     },
-  };
+    rollupOptions: {
+      external: ['solid-js'],
+      output: {
+        exports: 'named',
+        globals: {
+          'solid-js': 'solidJs',
+        },
+      },
+    },
+    emptyOutDir: false,
+  },
 });
